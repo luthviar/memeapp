241ca4cb819d9a2233dc643c1790fa6c2df144a8
luthfiabdurrahim <luthfi@learn-quran.co>
2021-07-08 14:40:10 +0700

    updated polished

--- memeapp/ViewController.swift
+++ memeapp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var photoLibraryButton: UIBarButtonItem!
     @IBOutlet weak var fontsButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var navigationBar: UINavigationBar!
@@ -21,16 +22,17 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     let DEFAULT_TOP_TEXT : String = "TOP"
     let DEFAULT_BOTTOM_TEXT : String = "BOTTOM"
     let DEFAULT_FIELD_TEXT_SIZE: CGFloat = 40
+    let DEFAULT_IMAGE: UIImage = UIImage(named: "chooseImage")!
     
-    
+    var memeObjects : [Meme] = []
     var memeTextAttributes : [NSAttributedString.Key: Any] = [
         NSAttributedString.Key.strokeColor : UIColor.black,
         NSAttributedString.Key.foregroundColor : UIColor.white,
-        NSAttributedString.Key.font : UIFont(name: "ComicSansMS", size: 40)!,
+        NSAttributedString.Key.font : UIFont(name: FontNames.impact.rawValue, size: 40)!,
         NSAttributedString.Key.strokeWidth : -4.0
     ] as [NSAttributedString.Key : Any]
     
-    var fontChoosed: String = "impact"
+    
     let availableFontsWithValue: [String:String] = [
         "Impact" : FontNames.impact.rawValue,
         "Times New Roman" : FontNames.timesNewRoman.rawValue,
@@ -51,11 +53,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI(isDefault: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        updateStyleTextField(textField: topTextField)
-        updateStyleTextField(textField: bottomTextField)
+        subscribeToKeyboardNotifications()
+        
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        unsubscribeToKeyboardNotifications()
+    }
+    
+    // MARK: FUNCTION HELPER
+    
+    func setupUI(isDefault: Bool = true) {
+        updateStyleTextField(textField: topTextField, isDefault: isDefault)
+        updateStyleTextField(textField: bottomTextField, isDefault: isDefault)
         topTextField.tag = 1
         bottomTextField.tag = 2
+        shareButton.isEnabled = false
+        imageView.image = DEFAULT_IMAGE
     }
     
     func updateStyleTextField(textField: UITextField, isDefault: Bool = true) {
@@ -70,30 +93,12 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         textField.delegate = self
     }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        //MAKE ALWAYS CAPITALS
-        textField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string.uppercased())
-
-            return false
-    }
-    
     func pickAnImageFromSource(source: UIImagePickerController.SourceType) {
         //Code To Pick An Image From Source
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
         pickerController.sourceType = source
         present(pickerController, animated: true, completion: nil)
-    }
-    
-    @IBAction func cameraButtonAction(_ sender: Any) {
-        pickAnImageFromSource(source: .camera)
-    }
-    @IBAction func photoLibraryAction(_ sender: Any) {
-        pickAnImageFromSource(source: .photoLibrary)
-    }
-    @IBAction func fontsButtonAction(_ sender: Any) {
-        showPopUpFonts("Pilih Font", message: "Let's choose your best font for meme!")
     }
     
     func showPopUpFonts(_ title: String, message: String) {
@@ -111,89 +116,19 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         self.present(alert, animated: true, completion: nil)
     }
     
+    // MARK: IMAGE PICKER CONTROLLER
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let image = info[.originalImage] as? UIImage {
             self.imageView.image = image
+            self.shareButton.isEnabled = true
         }
-        
         picker.dismiss(animated: true, completion: nil)
-        
     }
     
     func imagePickerControllerDidCancel(_: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        subscribeToKeyboardNotifications()
-        
-        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        unsubscribeToKeyboardNotifications()
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        if textField.text == DEFAULT_TOP_TEXT || textField.text == DEFAULT_BOTTOM_TEXT {
-            textField.text = ""
-        }
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField.text == "" {
-            switch TextFieldPosition(rawValue: textField.tag) {
-            case .top:
-                textField.text = DEFAULT_TOP_TEXT
-            case .bottom:
-                textField.text = DEFAULT_BOTTOM_TEXT
-            case .none:
-                textField.text = ""
-            }
-        }
-    }
-    
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        
-        return true
-    }
-    
-    @objc func keyboardWillShow(_ notification:Notification) {
-        if bottomTextField.isFirstResponder {
-            view.frame.origin.y = getKeyboardHeight(notification) * (-1)
-        }
-
-    }
-    
-    func subscribeToKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func keyboardWillHide(_ notification:Notification) {
-        if bottomTextField.isFirstResponder {
-            view.frame.origin.y = 0
-        }
-    }
-    
-    func unsubscribeToKeyboardNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
-
-        let userInfo = notification.userInfo
-        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-        return keyboardSize.cgRectValue.height
     }
     
     func generateMemedImage() -> UIImage {
@@ -217,9 +152,50 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     func save() {
         // Create The Meme
         let memedImage = generateMemedImage()
-        _ = Meme(top: topTextField.text!, bottom: bottomTextField.text!, image: imageView.image, memedImage:memedImage)
+        let memeImageObj = Meme(top: topTextField.text!, bottom: bottomTextField.text!, image: imageView.image, memedImage:memedImage)
+        memeObjects.append(memeImageObj)
+        // Save meme to array, for future use
+        print(memeObjects.count)
         
     }
+    
+    // MARK: TEXTFIELD PROTOCOLS DELEGATION
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        //MAKE ALWAYS CAPITALS
+        textField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string.uppercased())
+
+            return false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == DEFAULT_TOP_TEXT || textField.text == DEFAULT_BOTTOM_TEXT {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text == "" {
+            switch TextFieldPosition(rawValue: textField.tag) {
+            case .top:
+                textField.text = DEFAULT_TOP_TEXT
+            case .bottom:
+                textField.text = DEFAULT_BOTTOM_TEXT
+            case .none:
+                textField.text = ""
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    // MARK: IBACTION
     
     @IBAction func shareButtonAction(_ sender: Any) {
         let memedImage = generateMemedImage()
@@ -230,13 +206,50 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
         
         present(activityController, animated: true, completion: nil)
-        
+    }
+  
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        pickAnImageFromSource(source: .camera)
+    }
+    @IBAction func photoLibraryAction(_ sender: Any) {
+        pickAnImageFromSource(source: .photoLibrary)
+    }
+    @IBAction func fontsButtonAction(_ sender: Any) {
+        showPopUpFonts("Choose Font", message: "Let's choose your best font for the meme!")
     }
     
     @IBAction func cancelButtonAction(_ sender: Any) {
-        topTextField.text = DEFAULT_TOP_TEXT
-        bottomTextField.text = DEFAULT_BOTTOM_TEXT
-        self.imageView.image = nil
+        setupUI(isDefault: true)
     }
     
+    
+    // MARK: KEYBOARD HANDLING
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y = getKeyboardHeight(notification) * (-1)
+        }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
 }
